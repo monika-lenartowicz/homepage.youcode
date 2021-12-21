cd2e5b317331f667217fe42f690453bbf60f098f
Monika Lenartowicz <nika.nel@interia.pl>
2021-12-21 16:01:33 +0100

    change to function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,57 @@
-console.log("cześć!");
+// console.log("cześć!");
 
-let button = document.querySelector(".js-lastName");
-let lastName = document.querySelector(".lastName");
+// let button = document.querySelector(".js-lastName");
+// let lastName = document.querySelector(".lastName");
 
-button.addEventListener("click", () => {
-   if (lastName.innerText === ("")) {
-      lastName.innerText = " Monika Lenartowicz"
+// button.addEventListener("click", () => {
+//    if (lastName.innerText === ("")) {
+//       lastName.innerText = " Monika Lenartowicz"
+//    }
+// });
+
+// let themeButton = document.querySelector(".js-themeButton");
+// let darkTheme = document.querySelector(".darkTheme");
+// let body = document.querySelector(".body");
+// let dark = document.querySelector(".dark");
+
+// themeButton.addEventListener("click", () => {
+//    body.classList.toggle("darkTheme");
+//    dark.innerText = body.classList.contains("darkTheme") ? "Jasny"
+//       : "Ciemny";
+// });
+{
+   const welcome = () => {
+      console.log("Witam wszystkich na mojej stronie! 😃 ")
    }
-});
 
-let themeButton = document.querySelector(".js-themeButton");
-let darkTheme = document.querySelector(".darkTheme");
-let body = document.querySelector(".body");
-let dark = document.querySelector(".dark");
+   const toggleBackground = () => {
+      const body = document.querySelector(".body");
+      const dark = document.querySelector(".dark");
 
-themeButton.addEventListener("click", () => {
-   body.classList.toggle("darkTheme");
-   dark.innerText = body.classList.contains("darkTheme") ? "Jasny"
-      : "Ciemny";
-});
+
+      body.classList.toggle("darkTheme");
+      dark.innerText = body.classList.contains("darkTheme") ? "Jasny"
+         : "Ciemny";
+   };
+
+   const init = () => {
+      const themeButton = document.querySelector(".js-themeButton");
+      themeButton.addEventListener("click", (toggleBackground));
+
+      welcome();
+   }
+   init();
+}
+
+{
+   const button = document.querySelector(".js-lastName");
+   const lastName = document.querySelector(".lastName");
+
+   button.addEventListener("click", () => {
+      if (lastName.innerText === ("")) {
+         lastName.innerText = " Monika Lenartowicz"
+      }
+   });
+}
 
 
