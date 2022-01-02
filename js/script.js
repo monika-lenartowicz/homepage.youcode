050fd12e09b71f1616671c8cd8be9ec406ccfa4f
Monika Lenartowicz <nika.nel@interia.pl>
2022-01-02 15:10:40 +0100

    added emoji, deleted coments in js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// console.log("cześć!");
-
-// let button = document.querySelector(".js-lastName");
-// let lastName = document.querySelector(".lastName");
-
-// button.addEventListener("click", () => {
-//    if (lastName.innerText === ("")) {
-//       lastName.innerText = " Monika Lenartowicz"
-//    }
-// });
-
-// let themeButton = document.querySelector(".js-themeButton");
-// let darkTheme = document.querySelector(".darkTheme");
-// let body = document.querySelector(".body");
-// let dark = document.querySelector(".dark");
-
-// themeButton.addEventListener("click", () => {
-//    body.classList.toggle("darkTheme");
-//    dark.innerText = body.classList.contains("darkTheme") ? "Jasny"
-//       : "Ciemny";
-// });
 {
    const welcome = () => {
       console.log("Witam wszystkich na mojej stronie! 😃 ")
